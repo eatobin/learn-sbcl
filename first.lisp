b31fd8e8710f2fab83ae7b8255205182c85ab155
Eric Tobin <eatobin@gmail.com>
2018-08-11 16:22:11 -0500

    first

---
+++ first.lisp
@@ -0,0 +1,40 @@
+(let ((a 5)
+      (b 6))
+  (+ a b))
+
+(let ((x 7)
+      (y 77))
+  (* x y))
+
+(+ 4 4 )
+
+(flet ((f (n)
+         (+ n 10)))
+  (f 55))
+
+(flet ((f (n)
+         (+ n 10))
+       (g (n)
+         (- n 3)))
+  (g (f 55)))
+
+(labels ((a (n)
+           (+ n 5))
+         (b (n)
+           (+ (a n) 6)))
+  (b 10))
+
+(eq 'foo 'foo)
+(eq 'foo 'bar)
+
+(expt 53 53)
+
+(/ 4 6)
+(/ 4.0 6)
+
+(princ "Tutti Frutti")
+
+(defparameter *ml* '(one two three four five))
+
+(cadr *ml*)
+(cadddr *ml*)
