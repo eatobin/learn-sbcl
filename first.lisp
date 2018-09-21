a5e72bfdb5259541f8f2c13455bda294c76e5638
Eric Tobin <eatobin@gmail.com>
2018-09-21 16:55:06 -0500

    progress

--- first.lisp
+++ first.lisp
@@ -92,3 +92,16 @@
   'odd-number)
 
 ;; to page 56
+
+(defvar *arch-enemy* nil)
+(defun pudding-eater (person)
+  (cond ((eq person 'henry) (setf *arch-enemy* 'stupid-lisp-alien)
+         '(curse you lisp alien - you ate my pudding))
+        ((eq person 'johnny) (setf *arch-enemy* 'usless-old-johnny)
+         '(i hope you choked on my pudding johnny))
+        (t '(why you eat my pudding stranger?))))
+
+*arch-enemy*
+(pudding-eater 'johnny)
+(pudding-eater 'henry)
+(pudding-eater 'eric)
