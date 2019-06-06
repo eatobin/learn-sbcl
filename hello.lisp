d2ab106e90a75351e1905bb194844a43931f8ad9
Eric Tobin <eatobin@gmail.com>
2019-06-05 20:13:12 -0500

    and Eric

--- hello.lisp
+++ hello.lisp
@@ -1,5 +1,5 @@
 (defun hello-world ()
-  (format t "hello world"))
+  (format t "hello world and Eric!"))
 
 (defun make-cd (title artist rating ripped)
   (list :title title :artist artist :rating rating :ripped ripped))
